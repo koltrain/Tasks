3e90ba9265892b0aa4d0adaf1e92757e1766e898
GitTask <gittask@notused.com>
2021-02-05 09:11:30 +0300

    add styles
    -

---
+++ GPNStyles/Schemas/FileDetailV2/FileDetailV2.js
@@ -0,0 +1,14 @@
+define("FileDetailV2", [],
+	function() {
+	return {
+		properties: {},
+		attributes: {},
+		mixins: {},
+		messages: {},
+
+		methods: {},
+
+		diff: /**SCHEMA_DIFF*/[]
+		/**SCHEMA_DIFF*/
+	};
+});
